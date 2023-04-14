a0d7ba419d436dc76f32de7655556656545b1b4a
zetexztt <126012671+zetexztt@users.noreply.github.com>
2023-04-14 08:16:34 -0400

    Update event_tracker.py

--- event_tracker.py
+++ event_tracker.py
@@ -209,6 +209,10 @@ class OreEvent:
                     tracker_name = "GLOBAL"
                     if 'Spectral' in tier and 'Unfathomable' in tier:
                         print("OH SHIT")
+                    elif 'Spectral' in tier and 'Otherworldly' in tier:
+                        print("OH REALLY SHIT")
+                    elif 'Spectral' in tier and 'Zenith' in tier:
+                        print("OH EXTREMELY SHIT")
                     else:
                         tier = tier.replace("@everyone", "")
                 case EventType.BEGINNER:
