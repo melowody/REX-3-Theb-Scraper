3ab3e33c697656687f5c3cd42563007786f58101
zetexztt <126012671+zetexztt@users.noreply.github.com>
2023-06-04 14:42:51 -0400

    Update zetex_jr.py

--- zetex_jr.py
+++ zetex_jr.py
@@ -67,7 +67,7 @@ async def get_event(ctx: discord.AutocompleteContext):
             return ["None", "Sagittarius Quasar", "Legacy Flaeon", "Legacy Freon", "Legacy Poiseon", "Vaporwave Pulsar", "Legacy Codex", "Legacy Astatine", "Protoflare", "RGB Pulsar"]
 
 def check_owner(ctx):
-	return ctx.author.id in [797942648932794398, 190804082032640000]
+	return ctx.author.id in [797942648932794398, 190804082032640000, 609478516655915044]
 
 @zetex_jr.command()
 @commands.check(check_owner)
