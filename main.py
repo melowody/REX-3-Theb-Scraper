19774391218bd169fa8c8744f61ffe5cf06a91bf
Melody <melodydevofficial@gmail.com>
2023-08-26 15:52:28 -0400

    Fixing tabs

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import item_manager
 
 
 def main():
-    logging.basicConfig(filename='rex-log.txt', encoding='utf-8', level=logging.DEBUG)
-    zetex_jr.zetex_jr.run(item_manager.get_bot_token())
+	logging.basicConfig(filename='rex-log.txt', encoding='utf-8', level=logging.DEBUG)
+	zetex_jr.zetex_jr.run(item_manager.get_bot_token())
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- socket_based.py
+++ socket_based.py
@@ -3,14 +3,14 @@ import json
 
 
 class SocketBased:
-    
-    def __init__(self, socket: websocket.WebSocket):
-        self.socket = socket
-    
-    def send_json_request(self, request):
-        self.socket.send(json.dumps(request))
+	
+	def __init__(self, socket: websocket.WebSocket):
+		self.socket = socket
+	
+	def send_json_request(self, request):
+		self.socket.send(json.dumps(request))
 
-    def receive_json_response(self):
-        response = self.socket.recv()
-        if response:
-            return json.loads(response)
+	def receive_json_response(self):
+		response = self.socket.recv()
+		if response:
+			return json.loads(response)
