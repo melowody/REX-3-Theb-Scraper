8be09f431fc326719ae7dbed051c34459be5b2ca
zetexztt <126012671+zetexztt@users.noreply.github.com>
2023-08-27 18:58:31 -0400

    Update event_tracker.py

--- event_tracker.py
+++ event_tracker.py
@@ -98,7 +98,6 @@ class OreEvent:
                 self.special = SpecialType.NONE
         
         self.base_rarity = self.__embed["fields"][0]["value"].replace('1/', '')
-        self.base_rarity = re.sub("[^0-9]", "", self.base_rarity)
         
         self.blocks = int(self.__embed["fields"][1]["value"].replace(',',''))
         
@@ -119,7 +118,7 @@ class OreEvent:
         return self.rarity.value + self.special.value >= 9
 
     def get_base_rarity(self):
-        return "1 in " + str('{:,}'.format(int(self.base_rarity)))
+        return "1 in " + self.base_rarity
 
     def get_blocks(self):
         return '{:,}'.format(self.blocks)
