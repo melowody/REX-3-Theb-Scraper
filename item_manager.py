ddaab8a44696137454cb23a34d5a91dd5cf2b5dd
zetex <126012671+zetexztt@users.noreply.github.com>
2023-08-31 22:59:23 -0400

    Update item_manager.py

--- item_manager.py
+++ item_manager.py
@@ -1,48 +1,34 @@
 import json
 import os
 
-
 def get_bot_token():
     return os.getenv("BOT_TOKEN")
 
-
 def get_auth_token():
     return os.getenv("ZETEX_TOKEN")
-
 
 def get_tracker_bots():
     with open("info.json", "r") as f:
         return json.load(f)['TRACKER_BOTS']
 
-
 def get_color_names():
     with open("color_names.json", "r") as f:
         return json.load(f)
-
 
 def get_theb_dict():
     with open("theb_names.json", "r") as f:
         return json.load(f)
 
-
 def get_scoville_dict():
     with open("scoville_names.json", "r") as f:
-        return json.load(f)
-
-
-def get_zetex_dict():
-    with open("zetex_names.json", "r") as f:
         return json.load(f)
     
 def get_gooberville_dict():
     with open("gooberville.json", "r") as f:
         return json.load(f)
 
-
 def get_username(old_name, channel_id):
-    if channel_id == 0:
-        data = get_zetex_dict()
-    elif channel_id == 1:
+    if channel_id == 1:
         data = get_theb_dict()
     elif channel_id == 2:
         data = get_gooberville_dict()
