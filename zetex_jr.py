ad9263e42151817277bb3676e3db4936e9623537
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-01 19:39:32 -0400

    Update zetex_jr.py

--- zetex_jr.py
+++ zetex_jr.py
@@ -48,10 +48,10 @@ class TrackerBot(discord.Bot):
                 await self.get_channel(event_type.value).send(
                     event.format(event_type))
 
-    def send_error(self, error_data, do_restart):
+    async def send_error(self, error_data, do_restart):
         channel = self.get_channel(1076318101769039972)
         time.sleep(2)
-        channel.send("new error just dropped\n```" + error_data + "```")
+        await channel.send("new error just dropped\n```" + error_data + "```")
         if do_restart:
             os.system("cd ~ ; ./restart.sh")
 
