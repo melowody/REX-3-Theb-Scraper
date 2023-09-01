7e8c9315adbc4b53c155ee92f908328d5e255b02
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-01 19:23:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import item_manager
 
 def main():
     logging.basicConfig(filename='rex-log.txt', encoding='utf-8', level=logging.DEBUG)
-    zetex_jr.zetex_jr.run(item_manager.get_bot_token())
+    zetex_jr.TrackerBot.run(item_manager.get_bot_token())
 
 
 if __name__ == "__main__":
