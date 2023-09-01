8d940a7c01e946033e3165c4497b0171358e6a36
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-01 19:53:23 -0400

    Update event_tracker.py

--- event_tracker.py
+++ event_tracker.py
@@ -19,7 +19,7 @@ from functools import total_ordering
 def send_error(error_data, do_restart):
     print("sending error to logging channel on discord")
     discord_bot = zetex_jr.TrackerBot()
-    await discord_bot.send_error(error_data, do_restart)
+    discord_bot.send_error(error_data, do_restart)
 
 class SpecialType(Enum):
     NONE = 0
