68f63f5f721baca3915b7392919d07ce7bd9e82f
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-01 18:04:52 -0400

    Update zetex_jr.py

--- zetex_jr.py
+++ zetex_jr.py
@@ -100,8 +100,8 @@ async def restart(ctx):
     os.system("/root/restart.sh")
 
 
-async def error_logger(err):
+async def error_logger(err, do_restart):
     channel = self.get_channel(1076318101769039972)
-    await channel.send("this shit just happened: ```" + str({err}) + "``` attempting a restart")
-    os.system('cd ~ ; ./restart.sh')
-    
+    await channel.send("this shit just happened: ```" + str({err}) + "```")
+    if do_restart:
+        os.system('cd ~ ; ./restart.sh')
