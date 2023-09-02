da48a177eafc5a977e50993cdfa620ef31b740b6
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-01 21:08:28 -0400

    Update zetex_jr.py

--- zetex_jr.py
+++ zetex_jr.py
@@ -72,7 +72,7 @@ async def get_event(ctx: discord.AutocompleteContext):
 def check_owner(ctx):
     return ctx.author.id in [797942648932794398, 190804082032640000, 302920327699103744]
 
-@commands.command()
+@tracker_bot.command()
 @commands.check(check_owner)
 async def manual(ctx,
                  username: str,
@@ -97,7 +97,7 @@ async def manual(ctx,
     tracker_bot.et.queue.put(ore_event)
     await ctx.respond("manual ore successfully submitted :3", ephemeral=True)
 
-@commands.command()
+@tracker_bot.command()
 @commands.check(check_owner)
 async def restart(ctx):
     await ctx.respond("Restarting!")
