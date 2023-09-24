c52e362a2dd17f01cb3fa40fca7d5645753ee70c
zetex <126012671+zetexztt@users.noreply.github.com>
2023-09-23 22:43:59 -0400

    we're stupid

--- event_tracker.py
+++ event_tracker.py
@@ -338,7 +338,6 @@ class EventTracker(socket_based.SocketBased):
                 zetex_jr.send_error("event_tracker.py loop error 1: " + traceback.format_exc())
                 return
             try:
-                print(event)
                 if 'd' in event.keys() and type(event['d']) == dict and 'author' in event['d'].keys() and int(event['d']['author']['id']) in item_manager.get_tracker_bots() and 't' in event.keys() and event['t'] == 'MESSAGE_CREATE':
                     self.handle_event(event)
                 op_code = event['op']
